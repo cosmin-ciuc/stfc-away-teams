9edc0b685fdc4a1cc12f8e083b811db30774de98
Cosmin Petrenciuc <cpetrenciuc@computas.com>
2021-12-01 20:12:06 +0200

    new pictures

--- src/Components/AwayTeamMissionTrait.js
+++ src/Components/AwayTeamMissionTrait.js
@@ -6,11 +6,17 @@ import '../App.css';
 
 export class AwayTeamMissionTrait extends React.Component {
     render() {
+        const sortedOfficers = this.props.officers.map(officer => {
+            return {
+                ...officer,
+                order: officer.traits.indexOf(this.props.traitName)
+            };
+        }).sort((officer1, officer2) => officer1.order - officer2.order);
         return (
             <Stack gap={3}>
                 <div className="Trait">{this.props.traitName}</div>
                 <ListGroup>
-                    {this.props.officers.map((officer, i) => <ListGroupItem className={officer.rarity} key={i}>{officer.name}</ListGroupItem>)}
+                    {sortedOfficers.map((officer, i) => <ListGroupItem className={officer.rarity} key={i}><img src={'images/' + officer.name + '.png'} height="64"/><div>{officer.name}</div></ListGroupItem>)}
                 </ListGroup>
             </Stack>
         );
