ef0ecfe8dd7583806b84a13508a6083098954f67
Cosmin Petrenciuc <cpetrenciuc@computas.com>
2021-12-10 23:52:11 +0200

    changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders mission label', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+  const missionLabelElement = screen.getByText('Mission:');
+  expect(missionLabelElement).toBeInTheDocument();
 });
